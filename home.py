853c49eff74c93bcca81ef974897ba2536116910
emmanuelwpacheco-debug <emmanuel.wpacheco@gmail.com>
2026-01-22 13:23:26 -0300

    Create home.py

---
+++ home.py
@@ -0,0 +1,4 @@
+meu_sistema/
+├── Home.py              # Este será o seu Dashboard principal
+└── pages/               # Pasta obrigatoriamente com este nome
+    └── app.py  # Renomeie o seu código atual para este nome
